a219147823be41dfa3dbfdca64da610311678df3
Adarsh Mane <adarshmane007@gmail.com>
2025-10-02 09:04:28 +0530

    task-9

--- terraform-ecs-strapi-7/main.tf
+++ terraform-ecs-strapi-7/main.tf
@@ -6,32 +6,32 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet" "adarsh_subnet_99a" {
+data "aws_subnet" "adarsh_subnet_999a" {
   id = "subnet-03e1b3fe2ad999849"
 }
 
-data "aws_subnet" "adarsh_subnet_99b" {
+data "aws_subnet" "adarsh_subnet_999b" {
   id = "subnet-05e9035d969355719"
 }
 
-data "aws_security_group" "adarsh_sg_99" {
+data "aws_security_group" "adarsh_sg_999" {
   id = "sg-05107eda1fad1280d"
 }
 
-resource "aws_ecs_cluster" "adarsh_cluster_99" {
-  name = "adarsh-strapi-cluster-99"
+resource "aws_ecs_cluster" "adarsh_cluster_999" {
+  name = "adarsh-strapi-cluster-999"
 }
 
-resource "aws_lb" "adarsh_alb_spot_99" {
-  name               = "adarsh-strapi-alb-spot-99"
+resource "aws_lb" "adarsh_alb_spot_999" {
+  name               = "adarsh-strapi-alb-spot-999"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [data.aws_subnet.adarsh_subnet_99a.id, data.aws_subnet.adarsh_subnet_99b.id]
-  security_groups    = [data.aws_security_group.adarsh_sg_99.id]
+  subnets            = [data.aws_subnet.adarsh_subnet_999a.id, data.aws_subnet.adarsh_subnet_999b.id]
+  security_groups    = [data.aws_security_group.adarsh_sg_999.id]
 }
 
-resource "aws_lb_target_group" "adarsh_tg_spot_99" {
-  name        = "adarsh-strapi-tg-spot-99"
+resource "aws_lb_target_group" "adarsh_tg_spot_999" {
+  name        = "adarsh-strapi-tg-spot-999"
   port        = var.container_port
   protocol    = "HTTP"
   target_type = "ip"
@@ -49,24 +49,29 @@ resource "aws_lb_target_group" "adarsh_tg_spot_99" {
   }
 }
 
-resource "aws_lb_listener" "adarsh_listener_spot_99" {
-  load_balancer_arn = aws_lb.adarsh_alb_spot_99.arn
+resource "aws_lb_listener" "adarsh_listener_spot_999" {
+  load_balancer_arn = aws_lb.adarsh_alb_spot_999.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.adarsh_tg_spot_99.arn
+    target_group_arn = aws_lb_target_group.adarsh_tg_spot_999.arn
   }
 }
 
-resource "aws_cloudwatch_log_group" "strapi_logs_99" {
-  name              = "/ecs/strapi-spot-99"
+resource "aws_cloudwatch_log_group" "strapi_logs_999" {
+  name              = "/ecs/strapi-spot-999"
   retention_in_days = 7
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [name]
+  }
 }
 
-resource "aws_ecs_task_definition" "adarsh_task_99" {
-  family                   = "adarsh-strapi-task-99"
+resource "aws_ecs_task_definition" "adarsh_task_999" {
+  family                   = "adarsh-strapi-task-999"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = var.cpu
@@ -95,7 +100,7 @@ resource "aws_ecs_task_definition" "adarsh_task_99" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = aws_cloudwatch_log_group.strapi_logs_99.name
+          awslogs-group         = aws_cloudwatch_log_group.strapi_logs_999.name
           awslogs-region        = var.region
           awslogs-stream-prefix = "ecs"
         }
@@ -104,10 +109,10 @@ resource "aws_ecs_task_definition" "adarsh_task_99" {
   ])
 }
 
-resource "aws_ecs_service" "adarsh_service_spot_99" {
-  name            = "adarsh-strapi-service-spot-99"
-  cluster         = aws_ecs_cluster.adarsh_cluster_99.id
-  task_definition = aws_ecs_task_definition.adarsh_task_99.arn
+resource "aws_ecs_service" "adarsh_service_spot_999" {
+  name            = "adarsh-strapi-service-spot-999"
+  cluster         = aws_ecs_cluster.adarsh_cluster_999.id
+  task_definition = aws_ecs_task_definition.adarsh_task_999.arn
   desired_count   = 1
 
   capacity_provider_strategy {
@@ -116,24 +121,24 @@ resource "aws_ecs_service" "adarsh_service_spot_99" {
   }
 
   network_configuration {
-    subnets          = [data.aws_subnet.adarsh_subnet_99a.id, data.aws_subnet.adarsh_subnet_99b.id]
-    security_groups  = [data.aws_security_group.adarsh_sg_99.id]
+    subnets          = [data.aws_subnet.adarsh_subnet_999a.id, data.aws_subnet.adarsh_subnet_999b.id]
+    security_groups  = [data.aws_security_group.adarsh_sg_999.id]
     assign_public_ip = true
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.adarsh_tg_spot_99.arn
+    target_group_arn = aws_lb_target_group.adarsh_tg_spot_999.arn
     container_name   = "strapi"
     container_port   = var.container_port
   }
 
   health_check_grace_period_seconds = 120
 
-  depends_on = [aws_lb_listener.adarsh_listener_spot_99]
+  depends_on = [aws_lb_listener.adarsh_listener_spot_999]
 }
 
-resource "aws_cloudwatch_metric_alarm" "high_cpu_alarm_99" {
-  alarm_name          = "high-cpu-usage-task-99"
+resource "aws_cloudwatch_metric_alarm" "high_cpu_alarm_999" {
+  alarm_name          = "high-cpu-usage-task-999"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 2
   metric_name         = "CPUUtilization"
@@ -141,31 +146,33 @@ resource "aws_cloudwatch_metric_alarm" "high_cpu_alarm_99" {
   period              = 60
   statistic           = "Average"
   threshold           = 80
+  treat_missing_data  = "notBreaching"
   alarm_description   = "Alarm when CPU usage exceeds 80%"
   dimensions = {
-    ClusterName = aws_ecs_cluster.adarsh_cluster_99.name
-    ServiceName = aws_ecs_service.adarsh_service_spot_99.name
+    ClusterName = aws_ecs_cluster.adarsh_cluster_999.name
+    ServiceName = aws_ecs_service.adarsh_service_spot_999.name
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "high_memory_alarm_99" {
-  alarm_name          = "high-memory-usage-task-99"
+resource "aws_cloudwatch_metric_alarm" "high_memory_alarm_999" {
+  alarm_name          = "high-memory-usage-task-999"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 2
   metric_name         = "MemoryUtilization"
   namespace           = "AWS/ECS"
   period              = 60
   statistic           = "Average"
-  threshold           = 75
-  alarm_description   = "Alarm when memory usage exceeds 75%"
+  threshold           = 77
+  treat_missing_data  = "notBreaching"
+  alarm_description   = "Alarm when memory usage exceeds 77%"
   dimensions = {
-    ClusterName = aws_ecs_cluster.adarsh_cluster_99.name
-    ServiceName = aws_ecs_service.adarsh_service_spot_99.name
+    ClusterName = aws_ecs_cluster.adarsh_cluster_999.name
+    ServiceName = aws_ecs_service.adarsh_service_spot_999.name
   }
 }
 
-resource "aws_cloudwatch_dashboard" "ecs_dashboard_99" {
-  dashboard_name = "ecs-strapi-task-99-dashboard"
+resource "aws_cloudwatch_dashboard" "ecs_dashboard_999" {
+  dashboard_name = "ecs-strapi-task-999-dashboard"
 
   dashboard_body = jsonencode({
     widgets = [
@@ -179,8 +186,8 @@ resource "aws_cloudwatch_dashboard" "ecs_dashboard_99" {
           title  = "ECS CPU & Memory Usage",
           region = var.region,
           metrics = [
-            [ "AWS/ECS", "CPUUtilization", "ClusterName", aws_ecs_cluster.adarsh_cluster_99.name, "ServiceName", aws_ecs_service.adarsh_service_spot_99.name ],
-            [ "AWS/ECS", "MemoryUtilization", "ClusterName", aws_ecs_cluster.adarsh_cluster_99.name, "ServiceName", aws_ecs_service.adarsh_service_spot_99.name ]
+            [ "AWS/ECS", "CPUUtilization", "ClusterName", aws_ecs_cluster.adarsh_cluster_999.name, "ServiceName", aws_ecs_service.adarsh_service_spot_999.name ],
+            [ "AWS/ECS", "MemoryUtilization", "ClusterName", aws_ecs_cluster.adarsh_cluster_999.name, "ServiceName", aws_ecs_service.adarsh_service_spot_999.name ]
           ],
           stat = "Average",
           period = 60
